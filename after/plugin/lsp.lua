66d133d115b2fa53ee194f4c1e03fd33f1ad907a
michaelhass <4216942+michaelhass@users.noreply.github.com>
2023-05-02 13:13:20 +0900

    Add goo settings

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,7 +1,12 @@
-local lsp = require('lsp-zero').preset({})
+local lsp = require('lsp-zero').preset('recommended')
 
 lsp.on_attach(function(client, bufnr)
 	lsp.default_keymaps({buffer = bufnr})
+    lsp.buffer_autoformat()
 end)
+
+lsp.ensure_installed({
+    'gopls'
+})
 
 lsp.setup()
--- lua/michael/set.lua
+++ lua/michael/set.lua
@@ -24,3 +24,10 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
+
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = '*.go',
+  callback = function()
+    vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })
+  end
+})
